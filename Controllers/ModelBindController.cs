88943a4a2ff0f389f4681cf7e7a69921ff502236
yinghuaulei <yinghuaulei@yeah.net>
2022-07-29 15:30:18 +0800

    参数的模型绑定以及参数验证

---
+++ Controllers/ModelBindController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicDemo.Models;
+
+namespace MusicDemo.Controllers
+{
+    /// <summary>
+    /// 参数模型绑定
+    /// 参数模型验证
+    /// FromQuery
+    /// FromRoute
+    /// FromForm
+    /// FromBody
+    /// FromHandler
+    /// 默认情况下，ASP.NET Core是处理的JSON数据格式，如果想要使用其他类型，如xml，则需要通过制定Program.cs指定输入类型，然后在对应的方法上通过Consumes来执行Content-Type类型
+    /// </summary>
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ModelBindController : Controller
+    {
+        private readonly ILogger<ModelBindController> _logger;
+
+        public ModelBindController(ILogger<ModelBindController> logger)
+        {
+            _logger = logger;
+        }
+
+        #region 参数模型绑定
+
+        [HttpGet]
+        [Route("FromQuery")]
+        public string FromQuery([FromQuery] string param)
+        {
+            _logger.LogInformation("param:" + param);
+            return param;
+        }
+
+        // FromRoute 不能定义 Route，因为route必须是显式的，
+        // [Route("{param}")]
+        [HttpGet("{param}")]
+        public string FromRoute([FromRoute] string param)
+        {
+            _logger.LogInformation("param:" + param);
+            return param;
+        }
+
+        // [HttpGet]
+        // FromForm 不允许 httpget、httppost
+        // 已尝试可用 put、head
+        // 接收方式 未知
+        //[HttpHead]
+        //public void FromForm([FromForm] Music form)
+        //{
+        //    _logger.LogInformation($"id:{form.Id}, url: {form.Url}, title:{form.Url}");
+        //}
+
+        // [HttpGet]
+        // frombody 不允许 httpget
+        [HttpPost]
+        [Route("FromBody")]
+        public JsonResult FromBody([FromBody] Music body)
+        {
+            _logger.LogInformation($"id:{body.Id}, url: {body.Url}, title:{body.Url}");
+            return Json(body);
+        }
+
+        #endregion
+
+        #region 参数验证
+
+        [HttpPost]
+        [Route("CheckParam")]
+        public JsonResult CheckParam([FromBody] Music body)
+        {
+            _logger.LogInformation($"id:{body.Id}, url: {body.Url}, title:{body.Title}, length:{body.Length}");
+            return Json(body);
+        }
+
+        #endregion
+    }
+}
--- Models/Music.cs
+++ Models/Music.cs
@@ -1,11 +1,31 @@
-﻿namespace MusicDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicDemo.Models
 {
-    public class Music
+    public class Music : IValidatableObject // 自定义数据验证，继承接口 IValidatableObject
     {
         public long Id { get; set; }
 
         public string Url { get; set; } = string.Empty;
 
         public string Title { get; set; } = string.Empty;
+
+        // 数据验证
+        // 详情数据验证参见：https://docs.microsoft.com/zh-cn/dotnet/api/system.componentmodel.dataannotations?view=net-6.0
+        [Range(1, 1000000, ErrorMessage = "长度必须大于0，小于1000000")]
+        public int Length { get; set; }
+
+        /// <summary>
+        /// 歌名 ：Numb
+        /// </summary>
+        public string MusicName { get; set; } = string.Empty;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (MusicName == "Numb")
+            {
+                yield return new ValidationResult("I'm tired of being what you want me to be");
+            }
+        }
     }
 }
